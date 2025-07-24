5c176946e25e7ff40da5da563fcfe46f050cb45f
wing72 <xowkddl@gmail.com>
2025-07-24 09:28:03 +0900

    프로젝트 기본 상태

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ui_0724.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Home0724(),
+      home: Home0724(),
     );
   }
 }
