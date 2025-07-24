d67c85b669e901eca85b0ee6ab5c4398beb45335
wing72 <xowkddl@gmail.com>
2025-07-24 09:48:12 +0900

    WiP: 다이얼구현중

--- lib/ui_0724.dart
+++ lib/ui_0724.dart
@@ -1,9 +1,33 @@
-
 import 'package:flutter/material.dart';
 
 class Home0724 extends StatelessWidget {
+  
+  const Home0724({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(height: 250, width: double.infinity, color: Colors.red),
+          SizedBox(height: 50),
+          Container(
+            height: 400,
+            width: double.infinity,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'Hello, Flutter!',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+
+          // IconButton(onPressed: , icon: Icon(Icons.add)),
+        ],
+      ),
+    );
   }
 }
